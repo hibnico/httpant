a97f5f1a1ae0a949833d2d02a1d6a49cda5cfa7b
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2012-12-02 01:23:09 +0100

    add tests about multipart

--- src/test/java/org/apache/httpcomponents/ant/PostHttpClientTaskTest.java
+++ src/test/java/org/apache/httpcomponents/ant/PostHttpClientTaskTest.java
@@ -20,10 +20,22 @@ package org.apache.httpcomponents.ant;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.resources.PropertyResource;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -138,5 +150,115 @@ public class PostHttpClientTaskTest {
         task.execute();
 
         assertEquals(data, project.getProperty("response"));
+    }
+
+    @Test
+    public void testMultipartEntity() throws Exception {
+        final List<Part> parts = new ArrayList<Part>();
+        httpServerShell.setHandler(new AbstractHandler() {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException,
+                    ServletException {
+                baseRequest.setAttribute(Request.__MULTIPART_CONFIG_ELEMENT, new MultipartConfigElement((String) null));
+                parts.clear();
+                parts.addAll(baseRequest.getParts());
+            }
+        });
+
+        PostHttpClientTask task = new PostHttpClientTask();
+        task.setProject(project);
+        task.setUri(httpServerShell.getHttpServerUri());
+        task.setExpectedStatus(200);
+        MultipartEntityNode entity = new MultipartEntityNode();
+        FieldPartNode part = new FieldPartNode();
+        part.setName("field");
+        part.setValue("value");
+        entity.add(part);
+        task.add(entity);
+        task.execute();
+
+        assertEquals(1, parts.size());
+        assertEquals("field", parts.get(0).getName());
+        assertEquals("value", IOUtils.toString(parts.get(0).getInputStream()));
+
+        task = new PostHttpClientTask();
+        task.setProject(project);
+        task.setUri(httpServerShell.getHttpServerUri());
+        task.setExpectedStatus(200);
+        entity = new MultipartEntityNode();
+        part = new FieldPartNode();
+        part.setName("field");
+        part.setValue("value");
+        entity.add(part);
+        part = new FieldPartNode();
+        part.setName("field2");
+        part.setValue("value2");
+        part.setMimeType("text/xml");
+        part.setCharset("UTF-8");
+        entity.add(part);
+        task.add(entity);
+        task.execute();
+
+        assertEquals(2, parts.size());
+        assertEquals("field", parts.get(0).getName());
+        assertEquals("value", IOUtils.toString(parts.get(0).getInputStream()));
+        assertEquals("field2", parts.get(1).getName());
+        assertEquals("value2", IOUtils.toString(parts.get(1).getInputStream()));
+        assertEquals("text/xml; charset=UTF-8", parts.get(1).getContentType());
+
+        String data = "Ant and HttpClient rulez";
+        File dataFile = new File(tempDir, "data2.txt");
+        FileUtils.writeStringToFile(dataFile, data);
+
+        task = new PostHttpClientTask();
+        task.setProject(project);
+        task.setUri(httpServerShell.getHttpServerUri());
+        task.setExpectedStatus(200);
+        entity = new MultipartEntityNode();
+        FilePartNode filepart = new FilePartNode();
+        filepart.setName("datafile");
+        filepart.setFile(dataFile);
+        entity.add(filepart);
+        task.add(entity);
+        task.execute();
+
+        assertEquals(1, parts.size());
+        assertEquals("datafile", parts.get(0).getName());
+        assertEquals(data, IOUtils.toString(parts.get(0).getInputStream()));
+
+        String data3 = "Ant and HttpClient rulez";
+        File data3File = new File(tempDir, "data3.txt");
+        FileUtils.writeStringToFile(data3File, data3);
+
+        task = new PostHttpClientTask();
+        task.setProject(project);
+        task.setUri(httpServerShell.getHttpServerUri());
+        task.setExpectedStatus(200);
+        entity = new MultipartEntityNode();
+        filepart = new FilePartNode();
+        filepart.setName("datafile");
+        filepart.setFile(dataFile);
+        entity.add(filepart);
+        part = new FieldPartNode();
+        part.setName("field");
+        part.setValue("value");
+        entity.add(part);
+        filepart = new FilePartNode();
+        filepart.setName("data3file");
+        filepart.setFile(data3File);
+        filepart.setMimeType("text/xml");
+        filepart.setCharset("UTF-8");
+        entity.add(filepart);
+        task.add(entity);
+        task.execute();
+
+        assertEquals(3, parts.size());
+        assertEquals("field", parts.get(0).getName());
+        assertEquals("value", IOUtils.toString(parts.get(0).getInputStream()));
+        assertEquals("data3file", parts.get(1).getName());
+        assertEquals(data3, IOUtils.toString(parts.get(1).getInputStream()));
+        assertEquals("text/xml; charset=UTF-8", parts.get(1).getContentType());
+        assertEquals("datafile", parts.get(2).getName());
+        assertEquals(data, IOUtils.toString(parts.get(2).getInputStream()));
     }
 }
